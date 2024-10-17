13a6eac05fa776208bfb00ec8827c6481c5c27cc
Csaba Gaspar <cgaspar@accretivetg.com>
2024-10-17 06:45:16 +0200

    u

---
+++ emulators/cemu/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+CURRENT_ABSOLUTE_DIR=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  pwd -P
+)
+source $CURRENT_ABSOLUTE_DIR/../../vars.env
+DOWNLOAD_CACHE=$CURRENT_ABSOLUTE_DIR/../../download_cache
+
+
+#Copy image
+mkdir -p $APPS_DIR
+cp $DOWNLOAD_CACHE/$CEMU_APP_NAME $APPS_DIR
+chmod a+x $APPS_DIR/$CEMU_APP_NAME
+
+#cemu.sh
+cp $CURRENT_ABSOLUTE_DIR/configs/cemu.sh $APPS_DIR
+
+#~/.local/share/yuzu
+#mkdir -p $HOME/.local/share/yuzu/keys
+#cp $CURRENT_ABSOLUTE_DIR/files/*.keys $HOME/.local/share/yuzu/keys
+
+#~/.local/share/yuzu/nand/system/Contents/registered
+#mkdir -p $HOME/.local/share/yuzu/nand/system/Contents/registered
+#unzip $DOWNLOAD_CACHE/$YUZU_FIRMWARE_FILE -d $HOME/.local/share/yuzu/nand/system/Contents/registered
+
+#~/.config/yuzu
+#mkdir -p $HOME/.config/yuzu
+#mkdir -p $HOME/.config/yuzu/input
+#cp $CURRENT_ABSOLUTE_DIR/configs/qt-config.$SYSTEM.ini $HOME/.config/yuzu
+#mv $HOME/.config/yuzu/qt-config.$SYSTEM.ini $HOME/.config/yuzu/qt-config.ini
+
+#cp $CURRENT_ABSOLUTE_DIR/configs/input/steamdeck.ini $HOME/.config/yuzu/input
