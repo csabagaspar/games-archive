a12bcea04629b87ae2eac53b264f31935b273150
Csaba Gaspar <cgaspar@accretivetg.com>
2024-10-17 06:52:05 +0200

    u

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 ./frontends/es-de/install.sh
 ./emulators/yuzu/install.sh
 ./emulators/ppsspp/install.sh
+./emulators/cemu/install.sh
--- uninstall.sh
+++ uninstall.sh
@@ -3,3 +3,4 @@
 ./frontends/es-de/uninstall.sh
 ./emulators/yuzu/uninstall.sh
 ./emulators/ppsspp/uninstall.sh
+./emulators/cemu/uninstall.sh
